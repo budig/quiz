44f1545f90ebfe976099d2959880e2f36ca0276e
Budig <jigleb@gmail.com>
2015-06-24 14:54:52 +0200

    primera pagina y favicon

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
-var express = require('express');
+var express = require('express');   //importar middlewares
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index');   //Importar enrutadores
 
-var app = express();
+var app = express();    //Crear aplicacion
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));    //Se instala generador de
+app.set('view engine', 'ejs');        //vistas
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); //instalar middlewares
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes);   //agregar rutas
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error('Not Found');   //rutas no existentes: Error 404
     err.status = 404;
     next(err);
 });
@@ -37,8 +36,8 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
+    app.use(function(err, req, res, next) {   //Gestion de errores durante
+        res.status(err.status || 500);    //desarrollo
         res.render('error', {
             message: err.message,
             error: err
@@ -49,12 +48,12 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500);        //gestion de errores en produccion
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
 
-
+//exporta app para comando arranque
 module.exports = app;
