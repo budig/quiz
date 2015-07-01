a08fd73c360011b5e94d27d513b66d33467f9d33
Budig <jigleb@gmail.com>
2015-07-01 17:00:42 +0200

    Introduce partials

---
+++ README.md
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Quiz Juego de preguntas para pruebas con GITHUB</title>
+    </head>
+    <body>
+        <h1>Script en js para hacer pruebas con github.</h1>
+        <p>Juego de preguntas sin más.</p>
+    </body>
+
+</html>
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');   //Importar enrutadores
 
@@ -13,6 +14,7 @@ var app = express();    //Crear aplicacion
 app.set('views', path.join(__dirname, 'views'));    //Se instala generador de
 app.set('view engine', 'ejs');        //vistas
 
+app.use(partials());
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
